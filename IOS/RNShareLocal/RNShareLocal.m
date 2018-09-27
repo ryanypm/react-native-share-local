6e08f7455b23683f40191bfe02e5da8d5c59cd21
田亮 <543915174@qq.com>
2018-09-27 17:49:35 +0800

    fix: requires the main thread

--- IOS/RNShareLocal/RNShareLocal.m
+++ IOS/RNShareLocal/RNShareLocal.m
@@ -24,74 +24,78 @@ RCT_EXPORT_METHOD(message:(NSString *)title picUrl:(NSString *)picUrl excluded:(
     if(title){
         [objectsToShare addObject:title];
     }
-    UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:objectsToShare applicationActivities:nil];
-    activityVC.excludedActivityTypes = @[UIActivityTypeAirDrop];
     
-    UIViewController *rootController = UIApplication.sharedApplication.delegate.window.rootViewController;
-    
-    [rootController presentViewController:activityVC animated:YES completion:nil];
-    
-    NSMutableArray *excludedArray = [NSMutableArray arrayWithCapacity:15];
-    if ([excluded containsObject:@"UIActivityTypeMessage"]) {
-        [excludedArray addObject:UIActivityTypeMessage];
-    }
-    if ([excluded containsObject:@"UIActivityTypePostToFacebook"]) {
-        [excludedArray addObject:UIActivityTypePostToFacebook];
-    }
-    if ([excluded containsObject:@"UIActivityTypePostToTwitter"]) {
-        [excludedArray addObject:UIActivityTypePostToTwitter];
-    }
-    if ([excluded containsObject:@"UIActivityTypePostToWeibo"]) {
-        [excludedArray addObject:UIActivityTypePostToWeibo];
-    }
-    if ([excluded containsObject:@"UIActivityTypeMail"]) {
-        [excludedArray addObject:UIActivityTypeMail];
-    }
-    if ([excluded containsObject:@"UIActivityTypePrint"]) {
-        [excludedArray addObject:UIActivityTypePrint];
-    }
-    if ([excluded containsObject:@"UIActivityTypeCopyToPasteboard"]) {
-        [excludedArray addObject:UIActivityTypeCopyToPasteboard];
-    }
-    if ([excluded containsObject:@"UIActivityTypeAssignToContact"]) {
-        [excludedArray addObject:UIActivityTypeAssignToContact];
-    }
-    if ([excluded containsObject:@"UIActivityTypeSaveToCameraRoll"]) {
-        [excludedArray addObject:UIActivityTypeSaveToCameraRoll];
-    }
-    if ([excluded containsObject:@"UIActivityTypeAddToReadingList"]) {
-        [excludedArray addObject:UIActivityTypeAddToReadingList];
-    }
-    if ([excluded containsObject:@"UIActivityTypePostToFlickr"]) {
-        [excludedArray addObject:UIActivityTypePostToFlickr];
-    }
-    if ([excluded containsObject:@"UIActivityTypePostToVimeo"]) {
-        [excludedArray addObject:UIActivityTypePostToVimeo];
-    }
-    if ([excluded containsObject:@"UIActivityTypePostToTencentWeibo"]) {
-        [excludedArray addObject:UIActivityTypePostToTencentWeibo];
-    }
-    if ([excluded containsObject:@"UIActivityTypeAirDrop"]) {
-        [excludedArray addObject:UIActivityTypeAirDrop];
-    }
-    if ([excluded containsObject:@"UIActivityTypeOpenInIBooks"]) {
-        [excludedArray addObject:UIActivityTypeOpenInIBooks];
-    }
-    
-    
-    //NSLog(@"%@", excludedArray);
-    activityVC.excludedActivityTypes = excludedArray;
-    
-    activityVC.completionWithItemsHandler = ^(NSString *activityType, BOOL completed, NSArray *returnedItems, NSError *activityError) {
-        if (completed){
-            //NSLog(@"The Activity: %@ was completed", activityType);
-            callback(@[activityType]);
-        }else{
-            //NSLog(@"The Activity: %@ was NOT completed", activityType);
-            callback(@[@"fail"]);
+    dispatch_async(dispatch_get_main_queue(), ^{
+        
+        UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:objectsToShare applicationActivities:nil];
+        activityVC.excludedActivityTypes = @[UIActivityTypeAirDrop];
+        
+        UIViewController *rootController = UIApplication.sharedApplication.delegate.window.rootViewController;
+        
+        [rootController presentViewController:activityVC animated:YES completion:nil];
+        
+        NSMutableArray *excludedArray = [NSMutableArray arrayWithCapacity:15];
+        if ([excluded containsObject:@"UIActivityTypeMessage"]) {
+            [excludedArray addObject:UIActivityTypeMessage];
+        }
+        if ([excluded containsObject:@"UIActivityTypePostToFacebook"]) {
+            [excludedArray addObject:UIActivityTypePostToFacebook];
+        }
+        if ([excluded containsObject:@"UIActivityTypePostToTwitter"]) {
+            [excludedArray addObject:UIActivityTypePostToTwitter];
+        }
+        if ([excluded containsObject:@"UIActivityTypePostToWeibo"]) {
+            [excludedArray addObject:UIActivityTypePostToWeibo];
+        }
+        if ([excluded containsObject:@"UIActivityTypeMail"]) {
+            [excludedArray addObject:UIActivityTypeMail];
+        }
+        if ([excluded containsObject:@"UIActivityTypePrint"]) {
+            [excludedArray addObject:UIActivityTypePrint];
+        }
+        if ([excluded containsObject:@"UIActivityTypeCopyToPasteboard"]) {
+            [excludedArray addObject:UIActivityTypeCopyToPasteboard];
+        }
+        if ([excluded containsObject:@"UIActivityTypeAssignToContact"]) {
+            [excludedArray addObject:UIActivityTypeAssignToContact];
+        }
+        if ([excluded containsObject:@"UIActivityTypeSaveToCameraRoll"]) {
+            [excludedArray addObject:UIActivityTypeSaveToCameraRoll];
+        }
+        if ([excluded containsObject:@"UIActivityTypeAddToReadingList"]) {
+            [excludedArray addObject:UIActivityTypeAddToReadingList];
+        }
+        if ([excluded containsObject:@"UIActivityTypePostToFlickr"]) {
+            [excludedArray addObject:UIActivityTypePostToFlickr];
+        }
+        if ([excluded containsObject:@"UIActivityTypePostToVimeo"]) {
+            [excludedArray addObject:UIActivityTypePostToVimeo];
+        }
+        if ([excluded containsObject:@"UIActivityTypePostToTencentWeibo"]) {
+            [excludedArray addObject:UIActivityTypePostToTencentWeibo];
+        }
+        if ([excluded containsObject:@"UIActivityTypeAirDrop"]) {
+            [excludedArray addObject:UIActivityTypeAirDrop];
+        }
+        if ([excluded containsObject:@"UIActivityTypeOpenInIBooks"]) {
+            [excludedArray addObject:UIActivityTypeOpenInIBooks];
         }
         
-    };
+        
+        //NSLog(@"%@", excludedArray);
+        activityVC.excludedActivityTypes = excludedArray;
+        
+        activityVC.completionWithItemsHandler = ^(NSString *activityType, BOOL completed, NSArray *returnedItems, NSError *activityError) {
+            if (completed){
+                //NSLog(@"The Activity: %@ was completed", activityType);
+                callback(@[activityType]);
+            }else{
+                //NSLog(@"The Activity: %@ was NOT completed", activityType);
+                callback(@[@"fail"]);
+            }
+            
+        };
+    });
 }
 
 //分享连接
@@ -111,73 +115,75 @@ RCT_EXPORT_METHOD(link:(NSString *)title url:(NSString *)url picUrl:(NSString *)
         [objectsToShare addObject:[NSURL URLWithString:url]];
     }
     
-    UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:objectsToShare applicationActivities:nil];
-    
-    UIViewController *rootController = UIApplication.sharedApplication.delegate.window.rootViewController;
-    
-    [rootController presentViewController:activityVC animated:YES completion:nil];
-    
-    NSMutableArray *excludedArray = [NSMutableArray arrayWithCapacity:15];
-    if ([excluded containsObject:@"UIActivityTypeMessage"]) {
-        [excludedArray addObject:UIActivityTypeMessage];
-    }
-    if ([excluded containsObject:@"UIActivityTypePostToFacebook"]) {
-        [excludedArray addObject:UIActivityTypePostToFacebook];
-    }
-    if ([excluded containsObject:@"UIActivityTypePostToTwitter"]) {
-        [excludedArray addObject:UIActivityTypePostToTwitter];
-    }
-    if ([excluded containsObject:@"UIActivityTypePostToWeibo"]) {
-        [excludedArray addObject:UIActivityTypePostToWeibo];
-    }
-    if ([excluded containsObject:@"UIActivityTypeMail"]) {
-        [excludedArray addObject:UIActivityTypeMail];
-    }
-    if ([excluded containsObject:@"UIActivityTypePrint"]) {
-        [excludedArray addObject:UIActivityTypePrint];
-    }
-    if ([excluded containsObject:@"UIActivityTypeCopyToPasteboard"]) {
-        [excludedArray addObject:UIActivityTypeCopyToPasteboard];
-    }
-    if ([excluded containsObject:@"UIActivityTypeAssignToContact"]) {
-        [excludedArray addObject:UIActivityTypeAssignToContact];
-    }
-    if ([excluded containsObject:@"UIActivityTypeSaveToCameraRoll"]) {
-        [excludedArray addObject:UIActivityTypeSaveToCameraRoll];
-    }
-    if ([excluded containsObject:@"UIActivityTypeAddToReadingList"]) {
-        [excludedArray addObject:UIActivityTypeAddToReadingList];
-    }
-    if ([excluded containsObject:@"UIActivityTypePostToFlickr"]) {
-        [excludedArray addObject:UIActivityTypePostToFlickr];
-    }
-    if ([excluded containsObject:@"UIActivityTypePostToVimeo"]) {
-        [excludedArray addObject:UIActivityTypePostToVimeo];
-    }
-    if ([excluded containsObject:@"UIActivityTypePostToTencentWeibo"]) {
-        [excludedArray addObject:UIActivityTypePostToTencentWeibo];
-    }
-    if ([excluded containsObject:@"UIActivityTypeAirDrop"]) {
-        [excludedArray addObject:UIActivityTypeAirDrop];
-    }
-    if ([excluded containsObject:@"UIActivityTypeOpenInIBooks"]) {
-        [excludedArray addObject:UIActivityTypeOpenInIBooks];
-    }
-    
-    
-    //NSLog(@"%@", excludedArray);
-    activityVC.excludedActivityTypes = excludedArray;
-    
-    activityVC.completionWithItemsHandler = ^(NSString *activityType, BOOL completed, NSArray *returnedItems, NSError *activityError) {
-        if (completed){
-            //NSLog(@"The Activity: %@ was completed", activityType);
-            callback(@[activityType]);
-        }else{
-            //NSLog(@"The Activity: %@ was NOT completed", activityType);
-            callback(@[@"fail"]);
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:objectsToShare applicationActivities:nil];
+        
+        UIViewController *rootController = UIApplication.sharedApplication.delegate.window.rootViewController;
+        
+        [rootController presentViewController:activityVC animated:YES completion:nil];
+        
+        NSMutableArray *excludedArray = [NSMutableArray arrayWithCapacity:15];
+        if ([excluded containsObject:@"UIActivityTypeMessage"]) {
+            [excludedArray addObject:UIActivityTypeMessage];
+        }
+        if ([excluded containsObject:@"UIActivityTypePostToFacebook"]) {
+            [excludedArray addObject:UIActivityTypePostToFacebook];
+        }
+        if ([excluded containsObject:@"UIActivityTypePostToTwitter"]) {
+            [excludedArray addObject:UIActivityTypePostToTwitter];
+        }
+        if ([excluded containsObject:@"UIActivityTypePostToWeibo"]) {
+            [excludedArray addObject:UIActivityTypePostToWeibo];
+        }
+        if ([excluded containsObject:@"UIActivityTypeMail"]) {
+            [excludedArray addObject:UIActivityTypeMail];
+        }
+        if ([excluded containsObject:@"UIActivityTypePrint"]) {
+            [excludedArray addObject:UIActivityTypePrint];
+        }
+        if ([excluded containsObject:@"UIActivityTypeCopyToPasteboard"]) {
+            [excludedArray addObject:UIActivityTypeCopyToPasteboard];
+        }
+        if ([excluded containsObject:@"UIActivityTypeAssignToContact"]) {
+            [excludedArray addObject:UIActivityTypeAssignToContact];
+        }
+        if ([excluded containsObject:@"UIActivityTypeSaveToCameraRoll"]) {
+            [excludedArray addObject:UIActivityTypeSaveToCameraRoll];
+        }
+        if ([excluded containsObject:@"UIActivityTypeAddToReadingList"]) {
+            [excludedArray addObject:UIActivityTypeAddToReadingList];
+        }
+        if ([excluded containsObject:@"UIActivityTypePostToFlickr"]) {
+            [excludedArray addObject:UIActivityTypePostToFlickr];
+        }
+        if ([excluded containsObject:@"UIActivityTypePostToVimeo"]) {
+            [excludedArray addObject:UIActivityTypePostToVimeo];
+        }
+        if ([excluded containsObject:@"UIActivityTypePostToTencentWeibo"]) {
+            [excludedArray addObject:UIActivityTypePostToTencentWeibo];
+        }
+        if ([excluded containsObject:@"UIActivityTypeAirDrop"]) {
+            [excludedArray addObject:UIActivityTypeAirDrop];
+        }
+        if ([excluded containsObject:@"UIActivityTypeOpenInIBooks"]) {
+            [excludedArray addObject:UIActivityTypeOpenInIBooks];
         }
         
-    };
+        
+        //NSLog(@"%@", excludedArray);
+        activityVC.excludedActivityTypes = excludedArray;
+        
+        activityVC.completionWithItemsHandler = ^(NSString *activityType, BOOL completed, NSArray *returnedItems, NSError *activityError) {
+            if (completed){
+                //NSLog(@"The Activity: %@ was completed", activityType);
+                callback(@[activityType]);
+            }else{
+                //NSLog(@"The Activity: %@ was NOT completed", activityType);
+                callback(@[@"fail"]);
+            }
+            
+        };
+    });
 }
 
 //分享多图，最多九张
@@ -192,73 +198,76 @@ RCT_EXPORT_METHOD(pictures:(NSArray *)ImagesUrl excluded:(NSArray *)excluded cal
         [objectsToShare addObject:imageToShare];
         if(i == 8)break;
     }
-    UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:objectsToShare applicationActivities:nil];
     
-    UIViewController *rootController = UIApplication.sharedApplication.delegate.window.rootViewController;
-
-    NSMutableArray *excludedArray = [NSMutableArray arrayWithCapacity:15];
-    if ([excluded containsObject:@"UIActivityTypeMessage"]) {
-        [excludedArray addObject:UIActivityTypeMessage];
-    }
-    if ([excluded containsObject:@"UIActivityTypePostToFacebook"]) {
-        [excludedArray addObject:UIActivityTypePostToFacebook];
-    }
-    if ([excluded containsObject:@"UIActivityTypePostToTwitter"]) {
-        [excludedArray addObject:UIActivityTypePostToTwitter];
-    }
-    if ([excluded containsObject:@"UIActivityTypePostToWeibo"]) {
-        [excludedArray addObject:UIActivityTypePostToWeibo];
-    }
-    if ([excluded containsObject:@"UIActivityTypeMail"]) {
-        [excludedArray addObject:UIActivityTypeMail];
-    }
-    if ([excluded containsObject:@"UIActivityTypePrint"]) {
-        [excludedArray addObject:UIActivityTypePrint];
-    }
-    if ([excluded containsObject:@"UIActivityTypeCopyToPasteboard"]) {
-        [excludedArray addObject:UIActivityTypeCopyToPasteboard];
-    }
-    if ([excluded containsObject:@"UIActivityTypeAssignToContact"]) {
-        [excludedArray addObject:UIActivityTypeAssignToContact];
-    }
-    if ([excluded containsObject:@"UIActivityTypeSaveToCameraRoll"]) {
-        [excludedArray addObject:UIActivityTypeSaveToCameraRoll];
-    }
-    if ([excluded containsObject:@"UIActivityTypeAddToReadingList"]) {
-        [excludedArray addObject:UIActivityTypeAddToReadingList];
-    }
-    if ([excluded containsObject:@"UIActivityTypePostToFlickr"]) {
-        [excludedArray addObject:UIActivityTypePostToFlickr];
-    }
-    if ([excluded containsObject:@"UIActivityTypePostToVimeo"]) {
-        [excludedArray addObject:UIActivityTypePostToVimeo];
-    }
-    if ([excluded containsObject:@"UIActivityTypePostToTencentWeibo"]) {
-        [excludedArray addObject:UIActivityTypePostToTencentWeibo];
-    }
-    if ([excluded containsObject:@"UIActivityTypeAirDrop"]) {
-        [excludedArray addObject:UIActivityTypeAirDrop];
-    }
-    if ([excluded containsObject:@"UIActivityTypeOpenInIBooks"]) {
-        [excludedArray addObject:UIActivityTypeOpenInIBooks];
-    }
-    
-    
-    //NSLog(@"%@", excludedArray);
-    activityVC.excludedActivityTypes = excludedArray;
-    
-    activityVC.completionWithItemsHandler = ^(NSString *activityType, BOOL completed, NSArray *returnedItems, NSError *activityError) {
-        if (completed){
-            //NSLog(@"The Activity: %@ was completed", activityType);
-            callback(@[activityType]);
-        }else{
-            //NSLog(@"The Activity: %@ was NOT completed", activityType);
-            callback(@[@"fail"]);
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:objectsToShare applicationActivities:nil];
+        
+        UIViewController *rootController = UIApplication.sharedApplication.delegate.window.rootViewController;
+        
+        NSMutableArray *excludedArray = [NSMutableArray arrayWithCapacity:15];
+        if ([excluded containsObject:@"UIActivityTypeMessage"]) {
+            [excludedArray addObject:UIActivityTypeMessage];
+        }
+        if ([excluded containsObject:@"UIActivityTypePostToFacebook"]) {
+            [excludedArray addObject:UIActivityTypePostToFacebook];
+        }
+        if ([excluded containsObject:@"UIActivityTypePostToTwitter"]) {
+            [excludedArray addObject:UIActivityTypePostToTwitter];
+        }
+        if ([excluded containsObject:@"UIActivityTypePostToWeibo"]) {
+            [excludedArray addObject:UIActivityTypePostToWeibo];
+        }
+        if ([excluded containsObject:@"UIActivityTypeMail"]) {
+            [excludedArray addObject:UIActivityTypeMail];
+        }
+        if ([excluded containsObject:@"UIActivityTypePrint"]) {
+            [excludedArray addObject:UIActivityTypePrint];
+        }
+        if ([excluded containsObject:@"UIActivityTypeCopyToPasteboard"]) {
+            [excludedArray addObject:UIActivityTypeCopyToPasteboard];
+        }
+        if ([excluded containsObject:@"UIActivityTypeAssignToContact"]) {
+            [excludedArray addObject:UIActivityTypeAssignToContact];
+        }
+        if ([excluded containsObject:@"UIActivityTypeSaveToCameraRoll"]) {
+            [excludedArray addObject:UIActivityTypeSaveToCameraRoll];
+        }
+        if ([excluded containsObject:@"UIActivityTypeAddToReadingList"]) {
+            [excludedArray addObject:UIActivityTypeAddToReadingList];
+        }
+        if ([excluded containsObject:@"UIActivityTypePostToFlickr"]) {
+            [excludedArray addObject:UIActivityTypePostToFlickr];
+        }
+        if ([excluded containsObject:@"UIActivityTypePostToVimeo"]) {
+            [excludedArray addObject:UIActivityTypePostToVimeo];
+        }
+        if ([excluded containsObject:@"UIActivityTypePostToTencentWeibo"]) {
+            [excludedArray addObject:UIActivityTypePostToTencentWeibo];
+        }
+        if ([excluded containsObject:@"UIActivityTypeAirDrop"]) {
+            [excludedArray addObject:UIActivityTypeAirDrop];
+        }
+        if ([excluded containsObject:@"UIActivityTypeOpenInIBooks"]) {
+            [excludedArray addObject:UIActivityTypeOpenInIBooks];
         }
         
-    };
-    
-    [rootController presentViewController:activityVC animated:YES completion:nil];
+        
+        //NSLog(@"%@", excludedArray);
+        activityVC.excludedActivityTypes = excludedArray;
+        
+        activityVC.completionWithItemsHandler = ^(NSString *activityType, BOOL completed, NSArray *returnedItems, NSError *activityError) {
+            if (completed){
+                //NSLog(@"The Activity: %@ was completed", activityType);
+                callback(@[activityType]);
+            }else{
+                //NSLog(@"The Activity: %@ was NOT completed", activityType);
+                callback(@[@"fail"]);
+            }
+            
+        };
+        
+        [rootController presentViewController:activityVC animated:YES completion:nil];
+    });
 }
 
 @end
